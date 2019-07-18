507b7a68f6f3e89664f7fb67942ba5fbfa4fd16f
Omar Oubari <omaroubari@gmail.com>
2019-07-18 15:54:49 +0200

    added dataset (small) and time surfaces
    code still doesn't run

--- main.py
+++ main.py
@@ -1,49 +1,82 @@
+import sys
 import prosper 
 import numpy as np
 
 from utils.Cards_loader import Cards_loader
-from utils.Time_Surface_generators import Time_Surface_all
+from utils.Time_Surface_generators import Time_Surface_all, Time_Surface_event
 
 from prosper.em import EM
 from prosper.em.annealing import LinearAnnealing
 from prosper.em.camodels.bsc_et import BSC_ET
 import os 
 
-# Decide whether to run the sparse coding algorithm
-learning = False
+np.set_printoptions(threshold=sys.maxsize)
 
-# Each datapoint is of D = size*size
-size = 35
+#### PARAMETERS ####
 
-# Importing the pips dataset for testing purposes
-learning_set_length = 12
+learning = True # Decide whether to run the sparse coding algorithm
+ts_size = 11 # size of the time surfaces
+rec_size = 35 # size of the pip cards (square so dimension D = rec_size * rec_size)
+tau = 5000 # time constant for the construction of time surfaces
+polarities = 1 # number of polarities that we will use in the dataset (1 because polarities are not informative in the cards dataset)
+
+#### BUILDING THE LEARNING AND TESTING DATASETS ####
+learning_set_length = 2
 testing_set_length = 5
 
-data_folder = "/Users/omaroubari/Documents/Education/UPMC - PhD/Datasets/pips/"
+data_folder = "datasets/pips/"
 dataset_learning, labels_learning, dataset_testing, labels_testing = Cards_loader(data_folder, learning_set_length,testing_set_length)
 
-# Time surface of a full dataset 
-tss = []
-for i in range(learning_set_length):
-    tss.append(Time_Surface_all(xdim=size, ydim=size, timestamp=dataset_learning[i][0][-1], timecoeff=1000, dataset=dataset_learning[i], num_polarities=1, minv=0.1, verbose=True))
+# number of features  
+number_of_features = rec_size**2
 
-ts_test = []
-for i in range(testing_set_length):
-    ts_test.append([Time_Surface_all(xdim=size, ydim=size, timestamp=dataset_testing[i][0][-1], timecoeff=1000, dataset=dataset_testing[i], num_polarities=1, minv=0.1, verbose=False)])
+# setting up the learning dataset
+number_of_samples = len(dataset_learning)
+ts = np.zeros((number_of_samples,number_of_features,ts_size,ts_size))
+for recording in range(len(dataset_learning)):
+    for k in range(len(dataset_learning[recording][0])):
+        single_event = [dataset_learning[recording][0][k], dataset_learning[recording][1][k]]
+        time_surface = Time_Surface_event(xdim = ts_size,
+                                          ydim = ts_size,
+                                          event = single_event,
+                                          timecoeff = tau,
+                                          dataset = dataset_learning[recording],
+                                          num_polarities = polarities,
+                                          verbose = False)
 
-ts=np.random.randn(10,3,size,size) #let's pretend these are timesurfaces till we get it running
-# print(ts)
-# ts_test=np.random.randn(50,30,size,size) #let's pretend these are timesurfaces till we get it running
+        x = single_event[1][0]
+        y = single_event[1][1]
+        index = x + rec_size * y # 2D to 1D mapping
+        ts[recording][index] = time_surface
+
+# setting up the testing dataset
+number_of_samples = len(dataset_testing)
+ts_test = np.zeros((number_of_samples,number_of_features,ts_size,ts_size))
+for recording in range(len(dataset_testing)):
+    for k in range(len(dataset_testing[recording][0])):
+        single_event = [dataset_testing[recording][0][k], dataset_testing[recording][1][k]]
+
+        time_surface = Time_Surface_event(xdim = ts_size,
+                                          ydim = ts_size,
+                                          event = single_event,
+                                          timecoeff = tau,
+                                          dataset = dataset_testing[recording],
+                                          num_polarities = polarities,
+                                          verbose = False)
+
+        x = single_event[1][0]
+        y = single_event[1][1]
+        index = x + rec_size * y # 2D to 1D mapping
+        ts_test[recording][index] = time_surface
 
 #The code takes in data formated as Number of Samples x Number of Features so we need to reshape
-#Not sure of the timesurface format but we need to adapt it similarly
 ts = ts.reshape((ts.shape[0]*ts.shape[1],-1))
-# ts_test = ts_test.reshape((ts_test.shape[0]*ts_test.shape[1],-1))
+ts_test = ts_test.reshape((ts_test.shape[0]*ts_test.shape[1],-1))
 
 if learning:
     # Dimensionality of the model
     H=100     # let's start with 100
-    D=size**2    # dimensionality of observed data
+    D=ts_size**2    # dimensionality of observed data
 
     # Approximation parameters for Expectation Truncation (It has to be Hprime>=gamma)
     Hprime = 8
