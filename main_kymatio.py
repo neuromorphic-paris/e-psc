429a193efe778e38080281cce5c3daa7305b3f91
Georgios Exarchakis <gexarcha@gmail.com>
2019-07-29 18:19:18 +0200

    adds kymatio stuff and small chainges to main

--- main_kymatio.py
+++ main_kymatio.py
@@ -1,29 +1,23 @@
 import sys
-import prosper
 import numpy as np
 from scipy.stats import truncnorm
 
-from utils.Cards_loader import Cards_loader
+from utils.Cards_loader import Cards_loader as Cards
 from utils.Time_Surface_generators import Time_Surface_all, Time_Surface_event
 
-from prosper.em import EM
-from prosper.em.annealing import LinearAnnealing
-from prosper.em.camodels.bsc_et import BSC_ET
-from prosper.utils import create_output_path
-from prosper.utils.datalog import dlog, StoreToH5, TextPrinter, StoreToTxt
-
-
+from kymatio import Scattering2D
 import os
-
+import torch
+from time import time
 np.set_printoptions(threshold=sys.maxsize)
-output_path = create_output_path()
+
 
 #### PARAMETERS ####
 
 learning = True  # Decide whether to run the sparse coding algorithm
 classification = True  # Run classification
 
-ts_size = 11  # size of the time surfaces
+ts_size = 17  # size of the time surfaces
 # size of the pip cards (square so dimension D = rec_size * rec_size)
 rec_size = 35
 tau = 5000  # time constant for the construction of time surfaces
@@ -35,7 +29,7 @@ learning_set_length = 12
 testing_set_length = 5
 
 data_folder = "datasets/pips/"
-dataset_learning, labels_learning, dataset_testing, labels_testing = Cards_loader(
+dataset_learning, labels_training, dataset_testing, labels_testing = Cards(
     data_folder, learning_set_length, testing_set_length)
 
 #### BUILDING THE LEARNING DATASET ####
@@ -44,7 +38,7 @@ sizes_of_training_samples = [len(dataset_learning[j][0])
 number_of_samples = sum(sizes_of_training_samples)
 
 number_of_features = ts_size**2
-ts = np.zeros((number_of_samples, ts_size, ts_size))
+ts = np.zeros((number_of_samples, ts_size-1, ts_size-1))
 
 idx = 0
 training_labels = []
@@ -59,7 +53,7 @@ for recording in range(len(dataset_learning)):
                                           dataset=dataset_learning[recording],
                                           num_polarities=polarities,
                                           verbose=False)
-        ts[idx] = time_surface
+        ts[idx] = time_surface[:-1,:-1]
         training_labels.append(recording)
         idx += 1
 ts = ts.reshape((ts.shape[0], -1))
@@ -69,7 +63,7 @@ sizes_of_testing_samples = [len(dataset_testing[j][0])
                             for j in range(len(dataset_testing))]
 
 number_of_samples = sum(sizes_of_testing_samples)
-ts_test = np.zeros((number_of_samples, ts_size, ts_size))
+ts_test = np.zeros((number_of_samples, ts_size-1, ts_size-1))
 
 idx = 0
 test_labels = []
@@ -84,93 +78,55 @@ for recording in range(len(dataset_testing)):
                                           dataset=dataset_testing[recording],
                                           num_polarities=polarities,
                                           verbose=False)
-        ts_test[idx] = time_surface
+        ts_test[idx] = time_surface[:-1,:-1]
         test_labels.append(recording)
         idx += 1
 ts_test = ts_test.reshape((ts_test.shape[0], -1))
 
-#### RUNNING THE SPARSE CODING ALGORITHM ####
+# RUNNING THE SPARSE CODING ALGORITHM #
 if learning:
-    # Dimensionality of the model
-    H = 100     # let's start with 100
-    D = ts_size**2    # dimensionality of observed data
+    sz = int(np.sqrt(ts.shape[-1]))
+    t1 = time()
+    ts = torch.from_numpy(ts.reshape((ts.shape[0], 1, sz, sz)).astype(np.float32))
+    ts_test = torch.from_numpy(ts_test.reshape((ts_test.shape[0], 1, sz, sz)).astype(np.float32))
+    sc = Scattering2D(J=3, shape=(sz, sz))
+    train_coef = sc(ts)
+    test_coef = sc(ts_test)
+    t2 = time()
+    print("Scattering lasted {}s".format(t2-t1))
 
-    # Approximation parameters for Expectation Truncation (It has to be Hprime>=gamma)
-    Hprime = 8
-    gamma = 6
-
-    # Import and instantiate a model
-    from prosper.em.camodels.bsc_et import BSC_ET
-    model = BSC_ET(D, H, Hprime, gamma)
-
-    # Configure DataLogger
-    print_list = ('T', 'L', 'pi', 'sigma')
-    dlog.set_handler(print_list, TextPrinter)  # prints things to terminal
-    txt_list = ('T', 'L', 'pi', 'sigma')
-    dlog.set_handler(txt_list, StoreToTxt, output_path +
-                     '/results.txt')  # stores things in a txt file
-    h5_list = ('T', 'L', 'pi', 'sigma', 'W')
-    dlog.set_handler(h5_list, StoreToH5, output_path +
-                     '/results.h5')  # stores things in an h5 file
-
-    # Choose annealing schedule
-    from prosper.em.annealing import LinearAnnealing
-    anneal = LinearAnnealing(120)  # decrease
-    anneal['T'] = [(0, 5.), (.8, 1.)]
-    anneal['Ncut_factor'] = [(0, 0.), (0.5, 0.), (0.6, 1.)]
-    # anneal['Ncut_factor'] = [(0,0.),(0.7,1.)]
-    # anneal['Ncut_factor'] = [(0,0.),(0.7,1.)]
-    anneal['W_noise'] = [(0, np.std(ts) / 2.), (0.7, 0.)]
-    # anneal['pi_noise'] = [(0,0.),(0.2,0.1),(0.7,0.)]
-    anneal['anneal_prior'] = False
-
-    my_data = {'y': ts}
-    model_params = model.standard_init(my_data)
-    print("model defined")
-    em = EM(model=model, anneal=anneal)
-    em.data = my_data
-    em.lparams = model_params
-    em.run()
-    print("em finished")
-
-    my_test_data = {'y': ts_test}
-    res = model.inference(anneal, em.lparams, my_test_data)
-    sparse_codes = res['s'][:, 0, :]  # should be Number of samples x H
-    dlog.close()
 
 if classification:
-
-    my_train_data = {'y': ts}
-    res_train = model.inference(anneal, em.lparams, my_train_data)
 
     train_features = []
     start = 0
     for i in range(len(sizes_of_training_samples)):
         stop = start + sizes_of_training_samples[i]
-        train_features.append(res_train['s'][start:stop, 0, :].mean(0))
+        train_features.append(train_coef[start:stop, :].mean(0))
         start = stop
 
-    train_features = np.array(train_features)
-
-    my_test_data = {'y': ts_test}
-    res_test = model.inference(anneal, em.lparams, my_test_data)
+    train_features = torch.cat(train_features).numpy()
+    train_features = train_features.reshape((train_features.shape[0], -1))
 
     test_features = []
     start = 0
     for i in range(len(sizes_of_testing_samples)):
         stop = start + sizes_of_testing_samples[i]
-        test_features.append(res_test['s'][start:stop, 0, :].mean(0))
+        test_features.append(test_coef[start:stop, :].mean(0))
         start = stop
-    test_features = np.array(test_features)
+
+    test_features = torch.cat(test_features).numpy()
+    test_features = test_features.reshape((test_features.shape[0], -1))
 
     from sklearn.linear_model import LogisticRegression
     from sklearn import metrics
 
-    lreg = LogisticRegression(solver='lbfgs', multi_class='auto')
-    lreg.fit(train_features, labels_learning)
+    lreg = LogisticRegression()
+    lreg.fit(train_features, labels_training)
     predicted_labels = lreg.predict(test_features)
 
     print("Classification report for classifier %s:\n%s\n"
-          % (lreg, metrics.classification_report(labels_testing, predicted_labels)))
+          % (lreg, metrics.classification_report(labels_testing,
+                                                 predicted_labels)))
     print("Confusion matrix:\n%s" %
           metrics.confusion_matrix(labels_testing, predicted_labels))
--- utils/Time_Surface_generators.py
+++ utils/Time_Surface_generators.py
@@ -116,3 +116,58 @@ def Time_Surface_event(xdim, ydim, event, timecoeff, dataset, num_polarities, mi
 
     return tsurface
 
+## Time_Surface_event2: function that computes the Time_surface around a single event
+# =============================================================================
+# ydim,xdim : dimensions of the timesurface
+# event : single event defined as [timestamp, [x, y]]. It is the reference even 
+#         for the time surface
+# timecoeff : the time coeff expressing the time decay
+# dataset : dataset containing the events, a list where dataset[0] contains the 
+#           timestamps as microseconds, and dataset[1] contains [x,y] pixel positions 
+# num_polarities : total number of labels or polarities of the time surface 
+# minv : hardtreshold for the time surface, values smaller than minv will be
+#        removed from the result 
+# verbose : bool, if True, a graph of the surface will be plotted 
+#
+# tsurface : matrix returned with ldim as linear dimension
+# =============================================================================
+def Time_Surface_event2(xdim, ydim, event, timecoeff, dataset, num_polarities, minv=0.1, verbose=False):
+    tmpdata = [dataset[0].copy(), dataset[1].copy(), dataset[2].copy()]
+    #centering the dataset around the event
+    x0 = event[1][0]
+    y0 = event[1][1]
+    tmpdata[1][:,0] = tmpdata[1][:,0] - x0
+    tmpdata[1][:,1] = tmpdata[1][:,1] - y0
+    #taking only the timestamps before the reference 
+    timestamp = event[0]
+    ind = bisect_right(tmpdata[0],timestamp)
+    ind_subset = np.concatenate((np.ones(ind,bool), np.zeros(len(tmpdata[0])-(ind),bool)))
+    tmpdata = [tmpdata[0][ind_subset], tmpdata[1][ind_subset], tmpdata[2][ind_subset]]    
+    #removing all the timestamps that will generate values below minv
+    min_timestamp = timestamp + timecoeff*np.log(minv) #timestamps<min_timestamp WILL BE DISCARDED 
+    ind = bisect_left(tmpdata[0],min_timestamp)
+    ind_subset = np.concatenate((np.zeros(ind,bool), np.ones(len(tmpdata[0])-(ind),bool)))
+    tmpdata = [tmpdata[0][ind_subset], tmpdata[1][ind_subset], tmpdata[2][ind_subset]]     
+    #removing all events outside the region of interest defined as the xdim*ydim
+    # centered on the event
+    border = [np.floor(xdim/2),np.floor(ydim/2)]
+    ind_subset = ((tmpdata[1][:,0]>=-border[0]) & (tmpdata[1][:,0]<=border[0]) &
+        (tmpdata[1][:,1]>=-border[1]) & (tmpdata[1][:,1]<=border[1]))
+    tmpdata = [tmpdata[0][ind_subset], tmpdata[1][ind_subset], tmpdata[2][ind_subset]]   
+    tsurface_array = np.exp((tmpdata[0]-timestamp)/timecoeff)
+    #now i need to build a matrix that will represents my surface, i will take 
+    #only the highest value for each x and y as the other ar less informative
+    #and we want each layer be dependant on the timecoeff of the timesurface
+    #Note that exp is monotone and the timestamps are ordered, thus the last 
+    #values of the dataset will be the lowest too
+    tsurface = np.zeros([ydim,xdim*num_polarities])
+    offs = [np.int(np.floor(xdim/2)),np.int(np.floor(ydim/2))]
+    for i in range(len(tsurface_array)):
+        tsurface[tmpdata[1][i][1]+offs[1],tmpdata[1][i][0]+offs[0]+xdim*tmpdata[2][i]]=tsurface_array[i]  
+    #plot graphs if verbose is set "True"
+    if (verbose):
+        sns.heatmap(tsurface)
+        plt.show()
+
+    return tsurface
+
