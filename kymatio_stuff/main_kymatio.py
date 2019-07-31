2a988bda5258dddd952d6ddab8e2be65d4265efa
Georgios Exarchakis <gexarcha@gmail.com>
2019-07-31 16:21:45 +0200

    rearranges stuff

---
+++ kymatio_stuff/main_kymatio.py
@@ -0,0 +1,142 @@
+import sys
+import numpy as np
+from scipy.stats import truncnorm
+
+from utils.Cards_loader import Cards_loader as Cards
+from utils.Time_Surface_generators import Time_Surface_all, Time_Surface_event
+
+from kymatio import Scattering2D
+import os
+import torch
+from time import time
+np.set_printoptions(threshold=sys.maxsize)
+
+
+# PARAMETERS #
+
+learning = True  # Decide whether to run the sparse coding algorithm
+classification = True  # Run classification
+
+ts_size = 9  # size of the time surfaces
+# size of the pip cards (square so dimension D = rec_size * rec_size)
+rec_size = 35
+tau = 5000  # time constant for the construction of time surfaces
+# number of polarities that we will use in the dataset
+# (1 because polarities are not informative in the cards dataset)
+polarities = 1
+
+# IMPORTING DATASET #
+learning_set_length = 12
+testing_set_length = 5
+
+data_folder = "datasets/pips/"
+dataset_learning, labels_training, dataset_testing, labels_testing = Cards(
+    data_folder, learning_set_length, testing_set_length)
+
+# BUILDING THE LEARNING DATASET #
+sizes_of_training_samples = [len(dataset_learning[j][0])
+                             for j in range(len(dataset_learning))]
+number_of_samples = sum(sizes_of_training_samples)
+
+number_of_features = ts_size**2
+ts = np.zeros((number_of_samples, ts_size - 1, ts_size - 1))
+
+idx = 0
+training_labels = []
+for recording in range(len(dataset_learning)):
+    for k in range(len(dataset_learning[recording][0])):
+        single_event = [dataset_learning[recording]
+                        [0][k], dataset_learning[recording][1][k]]
+        time_surface = Time_Surface_event(xdim=ts_size,
+                                          ydim=ts_size,
+                                          event=single_event,
+                                          timecoeff=tau,
+                                          dataset=dataset_learning[recording],
+                                          num_polarities=polarities,
+                                          verbose=False)
+        ts[idx] = time_surface[:-1, :-1]
+        training_labels.append(recording)
+        idx += 1
+ts = ts.reshape((ts.shape[0], -1))
+
+# BUILDING THE TESTING DATASET #
+sizes_of_testing_samples = [len(dataset_testing[j][0])
+                            for j in range(len(dataset_testing))]
+
+number_of_samples = sum(sizes_of_testing_samples)
+ts_test = np.zeros((number_of_samples, ts_size - 1, ts_size - 1))
+
+idx = 0
+test_labels = []
+for recording in range(len(dataset_testing)):
+    for k in range(len(dataset_testing[recording][0])):
+        single_event = [dataset_testing[recording]
+                        [0][k], dataset_testing[recording][1][k]]
+        time_surface = Time_Surface_event(xdim=ts_size,
+                                          ydim=ts_size,
+                                          event=single_event,
+                                          timecoeff=tau,
+                                          dataset=dataset_testing[recording],
+                                          num_polarities=polarities,
+                                          verbose=False)
+        ts_test[idx] = time_surface[:-1, :-1]
+        test_labels.append(recording)
+        idx += 1
+ts_test = ts_test.reshape((ts_test.shape[0], -1))
+
+# RUNNING THE SPARSE CODING ALGORITHM #
+if learning:
+    sz = int(np.sqrt(ts.shape[-1]))
+    t1 = time()
+    ts = torch.from_numpy(ts.reshape((ts.shape[0], 1, sz, sz)).astype(np.float32))
+    ts_test = torch.from_numpy(ts_test.reshape((ts_test.shape[0], 1, sz, sz)).astype(np.float32))
+    sc = Scattering2D(J=2, shape=(sz, sz))
+    if torch.cuda.is_available():
+        print("Using Cuda")
+        sc.cuda()
+        ts.cuda()
+        ts_test.cuda()
+    else:
+        print("Not Using cuda")
+    train_coef = sc(ts)
+    test_coef = sc(ts_test)
+    if torch.cuda.is_available():
+        train_coef.cpu()
+        test_coef.cpu()
+    t2 = time()
+    print("Scattering lasted {}s".format(t2 - t1))
+
+if classification:
+
+    train_features = []
+    start = 0
+    for i in range(len(sizes_of_training_samples)):
+        stop = start + sizes_of_training_samples[i]
+        train_features.append(train_coef[start:stop, :].mean(0))
+        start = stop
+
+    train_features = torch.cat(train_features).numpy()
+    train_features = train_features.reshape((train_features.shape[0], -1))
+
+    test_features = []
+    start = 0
+    for i in range(len(sizes_of_testing_samples)):
+        stop = start + sizes_of_testing_samples[i]
+        test_features.append(test_coef[start:stop, :].mean(0))
+        start = stop
+
+    test_features = torch.cat(test_features).numpy()
+    test_features = test_features.reshape((test_features.shape[0], -1))
+
+    from sklearn.linear_model import LogisticRegression
+    from sklearn import metrics
+
+    lreg = LogisticRegression()
+    lreg.fit(train_features, labels_training)
+    predicted_labels = lreg.predict(test_features)
+
+    print("Classification report for classifier %s:\n%s\n"
+          % (lreg, metrics.classification_report(labels_testing,
+                                                 predicted_labels)))
+    print("Confusion matrix:\n%s" %
+          metrics.confusion_matrix(labels_testing, predicted_labels))
---
+++ prosper_stuff/main.py
@@ -0,0 +1,176 @@
+import sys
+import prosper
+import numpy as np
+from scipy.stats import truncnorm
+
+from utils.Cards_loader import Cards_loader
+from utils.Time_Surface_generators import Time_Surface_all, Time_Surface_event
+
+from prosper.em import EM
+from prosper.em.annealing import LinearAnnealing
+from prosper.em.camodels.bsc_et import BSC_ET
+from prosper.utils import create_output_path
+from prosper.utils.datalog import dlog, StoreToH5, TextPrinter, StoreToTxt
+
+
+import os
+
+np.set_printoptions(threshold=sys.maxsize)
+output_path = create_output_path()
+
+#### PARAMETERS ####
+
+learning = True  # Decide whether to run the sparse coding algorithm
+classification = True  # Run classification
+
+ts_size = 11  # size of the time surfaces
+# size of the pip cards (square so dimension D = rec_size * rec_size)
+rec_size = 35
+tau = 5000  # time constant for the construction of time surfaces
+# number of polarities that we will use in the dataset (1 because polarities are not informative in the cards dataset)
+polarities = 1
+
+#### IMPORTING DATASET ####
+learning_set_length = 12
+testing_set_length = 5
+
+data_folder = "../datasets/pips/"
+dataset_learning, labels_learning, dataset_testing, labels_testing = Cards_loader(
+    data_folder, learning_set_length, testing_set_length)
+
+#### BUILDING THE LEARNING DATASET ####
+sizes_of_training_samples = [len(dataset_learning[j][0])
+                             for j in range(len(dataset_learning))]
+number_of_samples = sum(sizes_of_training_samples)
+
+number_of_features = ts_size**2
+ts = np.zeros((number_of_samples, ts_size, ts_size))
+
+idx = 0
+training_labels = []
+for recording in range(len(dataset_learning)):
+    for k in range(len(dataset_learning[recording][0])):
+        single_event = [dataset_learning[recording]
+                        [0][k], dataset_learning[recording][1][k]]
+        time_surface = Time_Surface_event(xdim=ts_size,
+                                          ydim=ts_size,
+                                          event=single_event,
+                                          timecoeff=tau,
+                                          dataset=dataset_learning[recording],
+                                          num_polarities=polarities,
+                                          verbose=False)
+        ts[idx] = time_surface
+        training_labels.append(recording)
+        idx += 1
+ts = ts.reshape((ts.shape[0], -1))
+
+#### BUILDING THE TESTING DATASET ####
+sizes_of_testing_samples = [len(dataset_testing[j][0])
+                            for j in range(len(dataset_testing))]
+
+number_of_samples = sum(sizes_of_testing_samples)
+ts_test = np.zeros((number_of_samples, ts_size, ts_size))
+
+idx = 0
+test_labels = []
+for recording in range(len(dataset_testing)):
+    for k in range(len(dataset_testing[recording][0])):
+        single_event = [dataset_testing[recording]
+                        [0][k], dataset_testing[recording][1][k]]
+        time_surface = Time_Surface_event(xdim=ts_size,
+                                          ydim=ts_size,
+                                          event=single_event,
+                                          timecoeff=tau,
+                                          dataset=dataset_testing[recording],
+                                          num_polarities=polarities,
+                                          verbose=False)
+        ts_test[idx] = time_surface
+        test_labels.append(recording)
+        idx += 1
+ts_test = ts_test.reshape((ts_test.shape[0], -1))
+
+#### RUNNING THE SPARSE CODING ALGORITHM ####
+if learning:
+    # Dimensionality of the model
+    H = 100     # let's start with 100
+    D = ts_size**2    # dimensionality of observed data
+
+    # Approximation parameters for Expectation Truncation (It has to be Hprime>=gamma)
+    Hprime = 8
+    gamma = 6
+
+    # Import and instantiate a model
+    from prosper.em.camodels.bsc_et import BSC_ET
+    model = BSC_ET(D, H, Hprime, gamma)
+
+    # Configure DataLogger
+    print_list = ('T', 'L', 'pi', 'sigma')
+    dlog.set_handler(print_list, TextPrinter)  # prints things to terminal
+    txt_list = ('T', 'L', 'pi', 'sigma')
+    dlog.set_handler(txt_list, StoreToTxt, output_path +
+                     '/results.txt')  # stores things in a txt file
+    h5_list = ('T', 'L', 'pi', 'sigma', 'W')
+    dlog.set_handler(h5_list, StoreToH5, output_path +
+                     '/results.h5')  # stores things in an h5 file
+
+    # Choose annealing schedule
+    from prosper.em.annealing import LinearAnnealing
+    anneal = LinearAnnealing(120)  # decrease
+    anneal['T'] = [(0, 5.), (.8, 1.)]
+    anneal['Ncut_factor'] = [(0, 0.), (0.5, 0.), (0.6, 1.)]
+    # anneal['Ncut_factor'] = [(0,0.),(0.7,1.)]
+    # anneal['Ncut_factor'] = [(0,0.),(0.7,1.)]
+    anneal['W_noise'] = [(0, np.std(ts) / 2.), (0.7, 0.)]
+    # anneal['pi_noise'] = [(0,0.),(0.2,0.1),(0.7,0.)]
+    anneal['anneal_prior'] = False
+
+    my_data = {'y': ts}
+    model_params = model.standard_init(my_data)
+    print("model defined")
+    em = EM(model=model, anneal=anneal)
+    em.data = my_data
+    em.lparams = model_params
+    em.run()
+    print("em finished")
+
+    my_test_data = {'y': ts_test}
+    res = model.inference(anneal, em.lparams, my_test_data)
+    sparse_codes = res['s'][:, 0, :]  # should be Number of samples x H
+    dlog.close()
+
+if classification:
+
+    my_train_data = {'y': ts}
+    res_train = model.inference(anneal, em.lparams, my_train_data)
+
+    train_features = []
+    start = 0
+    for i in range(len(sizes_of_training_samples)):
+        stop = start + sizes_of_training_samples[i]
+        train_features.append(res_train['s'][start:stop, 0, :].mean(0))
+        start = stop
+
+    train_features = np.array(train_features)
+
+    my_test_data = {'y': ts_test}
+    res_test = model.inference(anneal, em.lparams, my_test_data)
+
+    test_features = []
+    start = 0
+    for i in range(len(sizes_of_testing_samples)):
+        stop = start + sizes_of_testing_samples[i]
+        test_features.append(res_test['s'][start:stop, 0, :].mean(0))
+        start = stop
+    test_features = np.array(test_features)
+
+    from sklearn.linear_model import LogisticRegression
+    from sklearn import metrics
+
+    lreg = LogisticRegression(solver='lbfgs', multi_class='auto')
+    lreg.fit(train_features, labels_learning)
+    predicted_labels = lreg.predict(test_features)
+
+    print("Classification report for classifier %s:\n%s\n"
+          % (lreg, metrics.classification_report(labels_testing, predicted_labels)))
+    print("Confusion matrix:\n%s" %
+          metrics.confusion_matrix(labels_testing, predicted_labels))
