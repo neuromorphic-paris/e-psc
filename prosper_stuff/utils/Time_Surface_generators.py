585fd50219f4653a8142aff6dda7a4e9222b1d3b
Georgios Exarchakis <georgios.exarchakis@uni-oldenburg.de>
2019-08-16 09:02:47 +0200

    changes time surfaces

--- prosper_stuff/utils/Time_Surface_generators.py
+++ prosper_stuff/utils/Time_Surface_generators.py
@@ -11,8 +11,8 @@ please note that these functions expect the dataset to be ordered from the
 lower timestamp to the highest
 """
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 import pandas as pd
 from scipy.stats import truncnorm
 from bisect import bisect_left, bisect_right
@@ -55,9 +55,9 @@ def Time_Surface_all(xdim, ydim, timestamp, timecoeff, dataset, num_polarities, 
         if tsurface[tmpdata[1][i][1],tmpdata[1][i][0]+xdim*tmpdata[2][i]]==0:
             tsurface[tmpdata[1][i][1],tmpdata[1][i][0]+xdim*tmpdata[2][i]]=tsurface_array[i]
     #plot graphs if verbose is set "True"
-    if (verbose):
-        sns.heatmap(tsurface)
-        plt.show()
+    #if (verbose):
+    #    sns.heatmap(tsurface)
+    #    plt.show()
 
     return tsurface
 
@@ -112,9 +112,9 @@ def Time_Surface_event(xdim, ydim, event, timecoeff, dataset, num_polarities, mi
     for i in range(len(tsurface_array)):
         tsurface[tmpdata[1][i][1]+offs[1],tmpdata[1][i][0]+offs[0]+xdim*tmpdata[2][i]]=tsurface_array[i]
     #plot graphs if verbose is set "True"
-    if (verbose):
-        sns.heatmap(tsurface)
-        plt.show()
+    #if (verbose):
+    #    sns.heatmap(tsurface)
+    #    plt.show()
 
     return tsurface
 
@@ -170,8 +170,8 @@ def Time_Surface_event2(xdim, ydim, event, sigma, dataset, num_polarities, minv=
     for i in range(len(tsurface_array)):
         tsurface[tmpdata[1][i][1]+offs[1],tmpdata[1][i][0]+offs[0]+xdim*tmpdata[2][i]]=tsurface_array[i]
     #plot graphs if verbose is set "True"
-    if (verbose):
-        sns.heatmap(tsurface)
-        plt.show()
+    #if (verbose):
+    #    sns.heatmap(tsurface)
+    #    plt.show()
 
     return tsurface
