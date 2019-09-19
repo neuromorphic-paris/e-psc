1e61af9e808b73d59cf370cbc6b52b2c4436d3ff
oubari <omaroubari@gmail.com>
2019-09-19 15:14:19 +0200

    my changes

--- prosper_stuff/main_nmnist2.py
+++ prosper_stuff/main_nmnist2.py
@@ -9,7 +9,7 @@ from utils.Time_Surface_generators import Time_Surface_all, Time_Surface_event
 from prosper.em import EM
 from prosper.em.annealing import LinearAnnealing
 from prosper.em.camodels.bsc_et import BSC_ET
-from prosper.em.camodels.dbsc_et import DBSC_ET
+#from prosper.em.camodels.dbsc_et import DBSC_ET
 from prosper.utils import create_output_path
 from prosper.utils.datalog import dlog, StoreToH5, TextPrinter, StoreToTxt
 import tables as tb
@@ -36,7 +36,7 @@ tau = 5000  # time constant for the construction of time surfaces
 polarities = 1  # number of polarities that we will use in the dataset (1 because polarities are not informative in the cards dataset)
 
 if resume:
-    output_path = "/users/ml/xoex6879/workspace/psc/prosper_stuff/output/main_nmnist.py.d1062928/"
+    output_path = "../../../results/main_nmnist.py.d1062928/"
 else:
     output_path = create_output_path()
 
@@ -48,7 +48,7 @@ to_scatter_train = None
 to_scatter_test = None
 if comm.rank == 0:
     #fh = tb.open_file("../datasets/nmnist_small.h5")
-    fh = tb.open_file("../datasets/nmnist_one_saccade.h5")
+    fh = tb.open_file("../../../datasets/nmnist_one_saccade.h5")
     #fh = tb.open_file("../datasets/nmnist.h5")
     dtr = [d.read().astype(np.int32) for d in fh.root.train]
     dte = [d.read().astype(np.int32) for d in fh.root.test]
@@ -225,6 +225,7 @@ if classification:
     train_features=[]
     train_labels2=[]
     start=0
+
     for i in range(len(train_rec_sizes)):
         stop = start + train_rec_sizes[i]
 
@@ -237,23 +238,27 @@ if classification:
         assert (this_l == this_l[0]).all()
         train_labels2.append(this_l[0])
         start = stop
-        if (comm.rank%7)==0:
+       # if (comm.rank%7)==0:
+            #print(comm.rank , ": {}%".format(100.*(i+1)/len(train_rec_sizes)))
+            #print("size {} -> {}".format(my_train_data['y'].shape[0],train_rec_sizes[i]))
 
-            print(comm.rank , ": {}%".format(100.*(i+1)/len(train_rec_sizes)))
-            print("size {} -> {}".format(my_train_data['y'].shape[0],train_rec_sizes[i]))
 
     train_features = np.array(train_features)
     train_labels = np.array(train_labels2)
-    
-    train_features_labels = comm.gather((train_features, train_labels))
+    print("converting to np")
+    train_features_labels = comm.gather((train_features, train_labels),root=0)
+    print("comm.gather")
     if comm.rank == 0:
+        print("parsing features and labels")
         train_features = np.concatenate([f[0] for f in train_features_labels])
         train_labels = np.concatenate([f[1] for f in train_features_labels])
+        print("saving to npz")
         np.savez(output_path+'/map_features_labels.npz',train_features=train_features, train_labels=train_labels)
 
     test_features = []
     test_labels2 = []
     start = 0
+    print("starting test")
     for i in range(len(test_rec_sizes)):
         stop = start + test_rec_sizes[i]
 
@@ -266,10 +271,10 @@ if classification:
         assert (this_l == this_l[0]).all()
         test_labels2.append(this_l[0])
         start = stop
-        if (comm.rank%7)==0:
+        #if (comm.rank%7)==0:
+           # print("TEST: ", comm.rank , ": {}%".format(100.*(i+1)/len(test_rec_sizes)))
+           # print("size {} -> {}".format(my_test_data['y'].shape[0],test_rec_sizes[i]))
 
-            print("TEST: ", comm.rank , ": {}%".format(100.*(i+1)/len(test_rec_sizes)))
-            print("size {} -> {}".format(my_test_data['y'].shape[0],test_rec_sizes[i]))
     test_features = np.array(test_features)
     test_labels = np.array(test_labels2)
 
@@ -280,7 +285,6 @@ if classification:
         test_labels = np.concatenate([f[1] for f in test_features_labels])
 
         np.savez(output_path+'/map_features_labels_all.npz',train_features=train_features, train_labels=train_labels,test_features=test_features,test_labels=test_labels)
-        
         print("Import stuff")
         from sklearn.linear_model import LogisticRegression
         from sklearn import metrics
